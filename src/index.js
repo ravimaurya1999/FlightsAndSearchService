b79f90974e4b8c28c0e23ecef59d95db3427ef8c
ravimaurya1999 <mauryaravikumar4@gmmail.com>
2023-11-07 18:49:42 +0530

    Added city model and migrations

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express =require("express");
+const bodyparser=require('body-parser');
+
+const {  PORT  } =require('./config/serverConfig');
+
+const setupAndStartServer= async () =>{
+       //create the express object.
+
+       const app =express();
+
+       app.use(bodyparser.json());
+       app.use(bodyparser.urlencoded({extended:true}));
+    
+       app.listen(PORT, ()  => {
+           console.log('Server started at ${PORT}');
+       });
+}
+
+setupAndStartServer();
