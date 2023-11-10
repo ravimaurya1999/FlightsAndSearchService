044d739c83543f8b5c5ca8a2b071986df3907f06
ravimaurya1999 <mauryaravikumar4@gmmail.com>
2023-11-10 15:53:05 +0530

    Added flights CRUD

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
 const CityController = require('../../controllers/city-controller');
+const FlightController = require('../../controllers/flight-controller');
 
 const router = express.Router();
-
 
 router.post('/city', CityController.create);
 router.delete('/city/:id', CityController.destroy);
@@ -11,9 +10,9 @@ router.get('/city/:id', CityController.get);
 router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update);
 
-
-
-
+router.get('/flights', FlightController.getAll);
+router.get('/flights/:id', FlightController.get);
+router.patch('/flights/:id', FlightController.update);
 
 
 
